175234c112dea5e935baffa6be81037114eb03f2
Cookielol777 <127045068+Cookielol777@users.noreply.github.com>
2025-04-28 16:16:34 +0200

    Update dual_launch.py

--- my_dual_nodes/launch/dual_launch.py
+++ my_dual_nodes/launch/dual_launch.py
@@ -4,12 +4,12 @@ import launch_ros.actions
 def generate_launch_description():
     return launch.LaunchDescription([
         launch_ros.actions.Node(
-            package='my_dual_nodes',
+            package='dual_uav_pid',
             executable='script1',
             name='node1'
         ),
         launch_ros.actions.Node(
-            package='my_dual_nodes',
+            package='dual_uav_pid',
             executable='script2',
             name='node2'
         ),
