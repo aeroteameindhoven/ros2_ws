2c186df5f26fbd58f7d7132812b9f159a5532cc7
Autopilot <autopilot@aeroteameindhoven.nl>
2025-06-06 16:59:42 +0200

    docker compos

--- run_camera_pipeline.sh
+++ run_camera_pipeline.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
 # Source ROS 2 and your workspace
-echo "Sourcing ROS 2 and workspace..."
-source /opt/ros/humble/setup.bash
-source /home/cam_ws/install/setup.bash
+#echo "Sourcing ROS 2 and workspace..."
+#source /opt/ros/humble/setup.bash
+#source /home/cam_ws/install/setup.bash
 
 echo "Starting v4l2_camera_node on /dev/video8..."
 #ros2 run v4l2_camera v4l2_camera_node --ros-args \
@@ -16,18 +16,28 @@ echo "Starting v4l2_camera_node on /dev/video8..."
 #  -r image_raw:=/camera/image_raw \
 #  -r camera_info:=/camera/camera_info
 
-ros2 run usb_cam usb_cam_node_exe --ros-args \
-  -p video_device:=/dev/video8 \
+#ros2 run usb_cam usb_cam_node_exe --ros-args \
+#  -p video_device:=/dev/video8 \
+#  -p camera_info_url:=file:///home/cam_ws/camera_calib3.yaml \
+#  -p autoexposure:=false \
+#  -p exposure:=330 \
+#  -p image_width:=960 \
+#  -p image_height:=600 \
+#  -p framerate:=80.0 \
+#  -r image_raw:=/camera/image_raw \
+#  -r camera_info:=/camera/camera_info &
+
+ros2 run v4l2_camera v4l2_camera_node --ros-args \
+  -p video_device:=/dev/v4l/by-id/usb-Arducam_Arducam_B0495__USB3_2.3MP__Arducam_20231205_0001-video-index0 \
   -p camera_info_url:=file:///home/cam_ws/camera_calib3.yaml \
-  -p autoexposure:=false \
-  -p exposure:=330 \
-  -p image_width:=960 \
-  -p image_height:=600 \
-  -p framerate:=80.0 \
+  -p auto_exposure:=1 \
+  -p exposure_time_absolute:=330 \
+  -p image_size:=[960,600] \
+  -p time_per_frame:=[1,80] \
   -r image_raw:=/camera/image_raw \
   -r camera_info:=/camera/camera_info &
 
-exit 1
+#exit 1
 sleep 2
 
 echo "Starting AprilTag node..."
